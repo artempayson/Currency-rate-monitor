13ba44f77dff458927b17cffcd12c2346e3bc87a
Artem <artempayson@gmail.com>
2016-12-06 18:21:23 +0300

    Repository class added and started being implemented Interface for Currency pairs was aded

---
+++ Logic/Repository.cs
@@ -0,0 +1,28 @@
+﻿using Logic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Repository
+    {
+        const string GetAllPairsURL = "https://btc-e.nz/api/3/info";
+        const string GetSpecifiedPairURL = "https://btc-e.nz/api/3/ticker"; //after / pair[s] should be specified
+        public async Task<List<ICurrencyPair>> GetAllPairs()
+        {
+            using (HttpClient hc = new HttpClient())
+            {
+                HttpResponseMessage response = await hc.GetAsync("https://btc-e.nz/api/3/info");
+                string responseString = await response.Content.ReadAsStringAsync();
+               // List<ICurrencyPair> allPairs = new List<ICurrencyPair>();
+
+            }
+            return null;
+        }
+
+    }
+}
