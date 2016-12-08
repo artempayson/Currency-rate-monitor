9b085449afbf4bc2473e1253691ebad5442d45f6
Kate <ekaterina.fofina@mail.ru>
2016-12-08 17:47:56 +0300

    Почти доделано построение графика

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Logic;
 using Logic.Model;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,18 +26,28 @@ namespace GUI
     {
         static Timer timer;
         Repository repository;
+        string[] axisXData;
+        decimal[] axisYData;
         public MainWindow()
         {
             InitializeComponent();
             repository = new Repository();
             timer = new Timer(3000);
             timer.Elapsed += UpdateGraph;
-            
+
+            Graph.ChartAreas.Add(new ChartArea("PlaceForGraph"));
+            Graph.Series.Add(new Series("CurrencyGraph"));
+            Graph.Series[0].ChartArea = "PlaceForGraph";
+            Graph.Series[0].ChartType = SeriesChartType.Line;
+            axisXData = new string[] { "a", "b", "c" };
+            axisYData = new decimal[] { 0.1m, 1.5m, 1.9m };
+
+
         }
 
         private void UpdateGraph(object sender, ElapsedEventArgs e)
         {
-        
+            Graph.Series[0].Points.DataBindXY(axisXData, axisYData);
         }
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
@@ -55,27 +67,40 @@ namespace GUI
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             CurrencyChooseComboBox.DisplayMemberPath = "FullName";
-           // if (CurrencyChooseComboBox.SelectedItem != null)
-               // Graph.Title = ((ICurrencyPair)CurrencyChooseComboBox.SelectedItem).FullName;
-
+            if (CurrencyChooseComboBox.SelectedItem != null)
+            {
+                Graph.Titles.Clear();
+                Title t = new Title(((ICurrencyPair)CurrencyChooseComboBox.SelectedItem).FullName);
+                t.Font = new Font("Segoe UI", 15);
+                Graph.Titles.Add(t);
+            }
         }
 
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
             CurrencyChooseComboBox.DisplayMemberPath = "ShortName";
-          //  if (CurrencyChooseComboBox.SelectedItem != null)
-              //  Graph.Title = ((ICurrencyPair)CurrencyChooseComboBox.SelectedItem).ShortName;
+            if (CurrencyChooseComboBox.SelectedItem != null)
+            {
+                Graph.Titles.Clear();
+                Title t = new Title(((ICurrencyPair)CurrencyChooseComboBox.SelectedItem).ShortName);
+                t.Font = new Font("Segoe UI", 15);
+                Graph.Titles.Add(t);
+            }
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
+
             Close();
         }
 
         private void CurrencyChooseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ICurrencyPair temp = (ICurrencyPair)CurrencyChooseComboBox.SelectedItem;
-           // Graph.Title = RadioButtonSN.IsChecked.Value ? temp.ShortName : temp.FullName;
+            Graph.Titles.Clear();
+            Title t = new Title(RadioButtonSN.IsChecked.Value ? temp.ShortName : temp.FullName);
+            t.Font = new Font("Segoe UI", 15);
+            Graph.Titles.Add(t);
 
         }
     }
